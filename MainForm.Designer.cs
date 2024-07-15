da1ff8c0aed6b61bafe506fa3748c4da9d2b8b9b
Obi-Wan Kenobi <Obi-Wan Kenobi@DEVESHI-02>
2024-07-15 21:52:28 +0700

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ReadWeblinkFiles
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnSelectFolder = new Button();
+            progressBar = new ProgressBar();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // btnSelectFolder
+            // 
+            btnSelectFolder.Location = new Point(364, 381);
+            btnSelectFolder.Name = "btnSelectFolder";
+            btnSelectFolder.Size = new Size(75, 30);
+            btnSelectFolder.TabIndex = 0;
+            btnSelectFolder.Text = "Go...";
+            btnSelectFolder.UseVisualStyleBackColor = true;
+            btnSelectFolder.Click += btnSelectFolder_Click;
+            // 
+            // progressBar
+            // 
+            progressBar.Location = new Point(39, 23);
+            progressBar.Name = "progressBar";
+            progressBar.Size = new Size(400, 23);
+            progressBar.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(39, 52);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.Size = new Size(400, 323);
+            textBox1.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(480, 434);
+            Controls.Add(textBox1);
+            Controls.Add(progressBar);
+            Controls.Add(btnSelectFolder);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Read Webloc Files";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnSelectFolder;
+        private ProgressBar progressBar;
+        private TextBox textBox1;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.IO;
+
+namespace ReadWeblinkFiles
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            using (var folderDialog = new FolderBrowserDialog())
+            {
+                DialogResult result = folderDialog.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderDialog.SelectedPath))
+                {
+                    string folderPath = folderDialog.SelectedPath;
+                    ProcessFolder(folderPath);
+                }
+            }
+        }
+
+        private void ProcessFolder(string folderPath)
+        {
+            string[] files = Directory.GetFiles(folderPath, "*.webloc");
+
+            List<string> extractedTexts = new List<string>();
+
+            progressBar.Maximum = files.Length;
+            progressBar.Value = 0;
+
+            foreach (string file in files)
+            {
+                string text = ExtractTextFromWebloc(file);
+                extractedTexts.Add(text);
+                progressBar.PerformStep();
+            }
+
+            WriteSortedTextToFile(extractedTexts);
+            MessageBox.Show("Text extraction and sorting completed!");
+        }
+
+        private string ExtractTextFromWebloc(string filePath)
+        {
+            textBox1.Text += filePath + Environment.NewLine + Environment.NewLine;
+            XDocument doc = XDocument.Load(filePath, LoadOptions.SetBaseUri);
+            XElement stringElement = doc.Descendants("string").FirstOrDefault();
+            return stringElement?.Value ?? string.Empty;
+        }
+
+        private void WriteSortedTextToFile(List<string> texts)
+        {
+            texts.Sort();
+
+            using (StreamWriter writer = new StreamWriter("output_sorted.txt"))
+            {
+                foreach (string text in texts)
+                {
+                    writer.WriteLine(text);
+                }
+            }
+        }
+    }
+}
